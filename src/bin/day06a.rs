2415cace60cc202f0b86d05965a0c46fba3e5b1b
Morten Munk <mortenmunk97@gmail.com>
2025-11-02 19:16:19 +0100

    wip

---
+++ src/bin/day06a.rs
@@ -0,0 +1,11 @@
+fn get_start_pos(matrix: Vec<Vec<char>>) -> (i32, i32) {
+    let x: i32 = 0;
+    let y: i32 = 0;
+}
+
+fn main() {
+    let content: Vec<Vec<char>> = include_str!("../../inputs/day06a.txt")
+        .lines()
+        .map(|x| x.chars().collect())
+        .collect();
+}
