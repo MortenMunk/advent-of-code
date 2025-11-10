4d323d3a768c559bce5acc083c4aed55dbf72c4a
Morten Munk <mortenmunk97@gmail.com>
2025-11-10 19:56:25 +0100

    progress

--- src/bin/day06a.rs
+++ src/bin/day06a.rs
@@ -1,12 +1,25 @@
-fn get_start_pos(matrix: &[Vec<char>]) -> (i32, i32) {
+#[derive(Debug)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+fn get_start_pos(matrix: &[Vec<char>]) -> Option<(i32, i32, Direction)> {
     for (y, row) in matrix.iter().enumerate() {
         for (x, &c) in row.iter().enumerate() {
-            if matches!(c, '^' | 'v' | '<' | '>') {
-                return (y as i32, x as i32);
-            }
+            let dir = match c {
+                '^' => Direction::North,
+                '<' => Direction::West,
+                '>' => Direction::East,
+                'v' => Direction::South,
+                _ => continue,
+            };
+            return Some((y as i32, x as i32, dir));
         }
     }
-    (0, 0)
+    None
 }
 
 fn main() {
@@ -14,5 +27,9 @@ fn main() {
         .lines()
         .map(|x| x.chars().collect())
         .collect();
-    let start = get_start_pos(&content);
+    if let Some((y, x, dir)) = get_start_pos(&content) {
+        println!("{y}, {x}, {dir:?}")
+    } else {
+        println!("No position found");
+    }
 }
