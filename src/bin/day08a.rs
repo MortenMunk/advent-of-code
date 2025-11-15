ac44cbd5778c99b64729592033ffbdc00dd1e602
Morten Munk <mortenmunk97@gmail.com>
2025-11-16 00:19:05 +0100

    wip

--- src/bin/day08a.rs
+++ src/bin/day08a.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 fn main() {
     let input = read_input();
     let mut antennas: HashMap<char, Vec<Cord>> = HashMap::new();
+    let mut counter: i64 = 0;
+
     for (row_index, row) in input.iter().enumerate() {
         for (col_index, ch) in row.iter().enumerate() {
             if *ch != '.' {
@@ -13,11 +15,36 @@ fn main() {
             }
         }
     }
+
+    for (key, vals) in antennas {
+        for i in 0..vals.len() {
+            for j in (i + 1)..vals.len() {
+                let p1 = &vals[i];
+                let p2 = &vals[j];
+
+                loop {
+                    let dr: i64 = p1.row as i64 - p2.row as i64;
+                    let dc: i64 = p1.col as i64 - p2.col as i64;
+
+                    if is_in_bounds(, ) {
+                        counter += 1;
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct Cord {
     row: i64,
     col: i64,
+}
+
+fn is_in_bounds(cord: &Cord, input: &Vec<Vec<char>>) -> bool {
+    cord.row >= 0
+        && cord.col >= 0
+        && (cord.row as usize) < input.len()
+        && (cord.col as usize) < input[0].len()
 }
 
 fn read_input() -> Vec<Vec<char>> {
