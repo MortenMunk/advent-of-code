0227e27c8f1e0789ae28722f8b605d4a1db458fe
Morten Munk <mortenmunk97@gmail.com>
2025-11-21 21:52:30 +0100

    working

--- src/bin/day10a.rs
+++ src/bin/day10a.rs
@@ -1,7 +1,12 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = read_input();
+    let start_points = init_start_points(&input);
+    let mut end_points: HashSet<Coord> = HashSet::new();
 }
 
+#[derive(Hash, PartialEq, Eq)]
 struct Coord {
     x: usize,
     y: usize,
@@ -14,8 +19,20 @@ fn read_input() -> Vec<Vec<char>> {
         .collect()
 }
 
-fn can_move_down(input: Vec<Vec<char>>, coord: Coord) -> bool {
-    if coord.y >= input.len() {
+fn init_start_points(input: &[Vec<char>]) -> HashSet<Coord> {
+    let mut set = HashSet::new();
+    for (y, row) in input.iter().enumerate() {
+        for (x, ch) in row.iter().enumerate() {
+            if *ch == '0' {
+                set.insert(Coord { x, y });
+            }
+        }
+    }
+    set
+}
+
+fn can_move_down(input: &[Vec<char>], coord: Coord) -> bool {
+    if coord.y + 1 >= input.len() {
         false
     } else {
         input[coord.y][coord.x].to_digit(10).unwrap() + 1
@@ -23,7 +40,7 @@ fn can_move_down(input: Vec<Vec<char>>, coord: Coord) -> bool {
     }
 }
 
-fn can_move_up(input: Vec<Vec<char>>, coord: Coord) -> bool {
+fn can_move_up(input: &[Vec<char>], coord: Coord) -> bool {
     if coord.y == 0 {
         false
     } else {
@@ -32,7 +49,7 @@ fn can_move_up(input: Vec<Vec<char>>, coord: Coord) -> bool {
     }
 }
 
-fn can_move_left(input: Vec<Vec<char>>, coord: Coord) -> bool {
+fn can_move_left(input: &[Vec<char>], coord: Coord) -> bool {
     if coord.x == 0 {
         false
     } else {
@@ -41,8 +58,8 @@ fn can_move_left(input: Vec<Vec<char>>, coord: Coord) -> bool {
     }
 }
 
-fn can_move_right(input: Vec<Vec<char>>, coord: Coord) -> bool {
-    if coord.x >= input[0].len() {
+fn can_move_right(input: &[Vec<char>], coord: Coord) -> bool {
+    if coord.x + 1 >= input[0].len() {
         false
     } else {
         input[coord.y][coord.x].to_digit(10).unwrap() + 1
