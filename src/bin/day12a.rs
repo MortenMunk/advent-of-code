b6052edb68dae5cca4f38c1f8b1f0c469e2c47d3
Morten Munk <mortenmunk97@gmail.com>
2025-11-28 22:14:11 +0100

    flood fill

--- src/bin/day12a.rs
+++ src/bin/day12a.rs
@@ -1,18 +1,27 @@
 fn main() {}
 
-fn dfs_rec(adj: &Vec<Vec<usize>>, visited: &mut Vec<bool>, s: usize, res: &mut Vec<usize>) {
-    visited[s] = true;
-    res.push(s);
-    for &i in &adj[s] {
-        if !visited[i] {
-            dfs_rec(adj, visited, i, res);
+fn dfs(grid: &Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>, r: usize, c: usize, plant: char) {
+    if visited[r][c] {
+        return;
+    }
+    visited[r][c] = true;
+
+    let h = grid.len();
+    let w = grid[0].len();
+    let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    for (dr, dc) in dirs {
+        let nr = r as isize + dr;
+        let nc = c as isize + dc;
+
+        if nr < 0 || nc < 0 || nr >= h as isize || nc >= w as isize {
+            continue;
+        }
+
+        let nr = nr as usize;
+        let nc = nc as usize;
+
+        if !visited[nr][nc] && grid[nr][nc] == plant {
+            dfs(grid, visited, nr, nc, plant);
         }
     }
-}
-
-fn dfs(adj: Vec<Vec<usize>>) -> Vec<usize> {
-    let mut visited: Vec<bool> = vec![false; adj.len()];
-    let mut res: Vec<usize> = Vec::new();
-    dfs_rec(&adj, &mut visited, 0, &mut res);
-    res
 }
