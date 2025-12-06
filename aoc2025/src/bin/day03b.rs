9d01875c3f1b2f168e7ef594b30e2df5ba59fa95
Morten Munk <mortenmunk97@gmail.com>
2025-12-06 13:07:40 +0100

    day2b

--- aoc2025/src/bin/day03b.rs
+++ aoc2025/src/bin/day03b.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 fn main() {
     let input: Vec<&str> = include_str!("../../inputs/day03.txt").lines().collect();
     let joltage_sum: u64 = input.iter().map(|x| joltage(x)).sum();
@@ -7,14 +5,32 @@ fn main() {
 }
 
 fn joltage(str: &str) -> u64 {
-    let str: Vec<u32> = str.chars().map(|x| x.to_digit(10).unwrap()).collect();
-    let mut queue: VecDeque<u32> = str.iter().take(11).cloned().collect();
-    for (i, val) in str.iter().enumerate() {
-        if i < str.len() - 11 {
-            break;
+    let digits: Vec<u32> = str.chars().map(|x| x.to_digit(10).unwrap()).collect();
+    let k = 12;
+    let mut stack: Vec<u32> = Vec::new();
+    let n = digits.len();
+
+    for (i, &d) in digits.iter().enumerate() {
+        // remove an element from the stack if we find a valid larger value
+        while !stack.is_empty() && *stack.last().unwrap() < d && stack.len() + (n - i - 1) >= k {
+            stack.pop();
         }
-        if !queue.is_empty() || queue.back() > val {}
+
+        // fill the stack if it is less than 12 long
+        if stack.len() < k {
+            stack.push(d);
+        }
     }
+    vec_to_num(&stack)
+}
+
+fn vec_to_num(queue: &[u32]) -> u64 {
+    queue
+        .iter()
+        .map(|d| char::from_digit(*d, 10).unwrap())
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -23,19 +39,19 @@ mod tests {
 
     #[test]
     fn test1() {
-        let input = "1199999999991";
-        assert_eq!(joltage(input), 99999999991);
+        let input = "91199999999991";
+        assert_eq!(joltage(input), 999999999991);
     }
 
     #[test]
     fn test2() {
-        let input = "999999999990";
-        assert_eq!(joltage(input), 99999999999);
+        let input = "99999999999990";
+        assert_eq!(joltage(input), 999999999999);
     }
 
     #[test]
     fn test3() {
-        let input = "9999991111199922229333390";
-        assert_eq!(joltage(input), 99999999999);
+        let input = "99999991111199922229333390";
+        assert_eq!(joltage(input), 999999999999);
     }
 }
