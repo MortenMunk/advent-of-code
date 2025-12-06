f3e0e7bf592c7704544b056215d1dfdf1960fb2d
Morten Munk <mortenmunk97@gmail.com>
2025-12-06 01:03:29 +0100

    day 3 solved

--- aoc2025/src/bin/day03a.rs
+++ aoc2025/src/bin/day03a.rs
@@ -8,11 +8,12 @@ fn joltage(str: &str) -> u32 {
     let str: Vec<char> = str.chars().collect();
     let mut tens = 0;
     let mut ones = 0;
-    let ref_point = 0;
+    let mut ref_point = 0;
     for (i, val) in str.iter().enumerate() {
         let val = val.to_digit(10).unwrap();
-        if val > tens && i < str.len() {
+        if val > tens && i < str.len() - 1 {
             tens = val;
+            ref_point = i;
         }
     }
 
@@ -23,4 +24,21 @@ fn joltage(str: &str) -> u32 {
         }
     }
     10 * tens + ones
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let input = "111111192";
+        assert_eq!(joltage(input), 92);
+    }
+
+    #[test]
+    fn test2() {
+        let input = "511119";
+        assert_eq!(joltage(input), 59);
+    }
 }
