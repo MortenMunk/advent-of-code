69003b0ba57cd994ba18ca9b199cdb69501ded86
Morten Munk <mortenmunk97@gmail.com>
2025-12-10 21:02:18 +0100

    day6a done

--- aoc2025/src/bin/day06a.rs
+++ aoc2025/src/bin/day06a.rs
@@ -3,5 +3,26 @@ fn main() {
         .lines()
         .map(|x| x.split_whitespace().collect())
         .collect();
-    for col in 0..input[0].len() - 1 {}
+
+    let cols = input[0].len();
+    let rows = input.len();
+
+    let mut sum: u64 = 0;
+    let mut sub_sum: u64 = 0;
+
+    (0..cols).for_each(|col| {
+        if input[rows - 1][col] == "+" {
+            sub_sum = 0;
+            (0..rows - 1).for_each(|row| {
+                sub_sum += input[row][col].parse::<u64>().unwrap_or(0);
+            });
+        } else {
+            sub_sum = 1;
+            (0..rows - 1).for_each(|row| {
+                sub_sum *= input[row][col].parse::<u64>().unwrap_or(0);
+            });
+        }
+        sum += sub_sum;
+    });
+    println!("{}", sum);
 }
