fb2e028af9dd5c7bb4ba319c47279c064890d41b
Morten Munk <mortenmunk97@gmail.com>
2025-12-13 00:07:49 +0100

    7a done

--- aoc2025/src/bin/day07a.rs
+++ aoc2025/src/bin/day07a.rs
@@ -3,12 +3,32 @@ fn main() {
         .lines()
         .map(|line| line.chars().collect())
         .collect();
-    input
+    let line_len: usize = input[0].len();
+    let mut beam_mask = init_beam_mask(line_len);
+    let counter: u64 = input
         .iter()
-        .for_each(|line| {
+        .map(|line| {
+            let mut split_count: u64 = 0;
             for (i, val) in line.iter().enumerate() {
-                // func here
+                if *val == '^' && beam_mask[i] == 1 {
+                    split_count += 1;
+                    if i > 0 {
+                        beam_mask[i - 1] = 1
+                    }
+                    if i < line_len {
+                        beam_mask[i + 1] = 1;
+                    }
+                    beam_mask[i] = 0;
+                }
             }
+            split_count
         })
         .sum();
+    println!("{}", counter);
+}
+
+fn init_beam_mask(input_len: usize) -> Vec<u8> {
+    let mut bm = vec![0; input_len];
+    bm[input_len / 2] = 1;
+    bm
 }
