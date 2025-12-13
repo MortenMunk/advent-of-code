aff02967937afaef3bba97a5f11f2b6909126b84
Morten Munk <mortenmunk97@gmail.com>
2025-12-13 22:02:07 +0100

    day08a progress

--- aoc2025/src/bin/day08a.rs
+++ aoc2025/src/bin/day08a.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 type Input = Vec<Vec<u64>>;
+type DistList = Vec<(u64, usize, usize)>;
 
 fn main() {
     let input: Input = include_str!("../../inputs/day08.txt")
@@ -18,18 +19,19 @@ fn euclid_dist(irow: &[u64], jrow: &[u64]) -> u64 {
     (irow[0] - jrow[0]).pow(2) + (irow[1] - jrow[1]).pow(2) + (irow[2] - jrow[2]).pow(2)
 }
 
-fn init_distances(input: Input) -> HashMap<String, u64> {
+fn init_distances(input: Input) -> DistList {
     let mut visited: HashSet<usize> = HashSet::new();
-    let mut distances: HashMap<String, u64> = HashMap::new();
+    let mut distances: DistList = Vec::new();
     for (i, irow) in input.iter().enumerate() {
         for (j, jrow) in input.iter().enumerate() {
             if i == j || visited.contains(&j) {
                 continue;
             }
             let dist = euclid_dist(irow, jrow);
-            distances.insert(format!("{}-{}", i, j), dist);
+            distances.push((dist, i, j));
         }
         visited.insert(i);
     }
+    distances.sort_unstable_by_key(|(dist, _, _)| *dist);
     distances
 }
