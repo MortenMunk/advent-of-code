8cfcae56d45ef20f37b1eb2494f8f8bbdd702fa0
Morten Munk <mortenmunk97@gmail.com>
2025-12-18 20:28:40 +0100

    WIP

--- aoc2025/src/bin/day10a.rs
+++ aoc2025/src/bin/day10a.rs
@@ -6,13 +6,14 @@ struct LineData {
 }
 
 fn main() {
-    let input = include_str!("../../inputs/day10.txt")
+    let input: Vec<LineData> = include_str!("../../inputs/day10.txt")
         .lines()
         .map(parseline)
         .collect();
 }
 
-
-fn parseline(line) -> LineData {
-    // TODO
+fn parseline(line: &str) -> LineData {
+    let pattern_start = line.find('[').unwrap() + 1;
+    let pattern_end = line.find(']').unwrap();
+    let pattern = line[pattern_start..pattern_end].to_string();
 }
