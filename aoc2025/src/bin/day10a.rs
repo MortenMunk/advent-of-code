e3b338ec5f748ce9c6c20aa689dd5f5706d01bca
Morten Munk <mortenmunk97@gmail.com>
2025-12-20 22:18:27 +0100

    day10a

--- aoc2025/src/bin/day10a.rs
+++ aoc2025/src/bin/day10a.rs
@@ -10,7 +10,51 @@ fn main() {
         .lines()
         .map(parseline)
         .collect();
-    println!("{:?}", input[0]);
+
+    let counter: u64 = input.iter().map(min_presses).sum();
+    println!("{}", counter);
+}
+
+fn min_presses(linedata: &LineData) -> u64 {
+    let mask = init_square_bitmask(&linedata.square);
+    let buttons: Vec<u64> = linedata.paren.iter().map(|x| build_mask(x)).collect();
+
+    let mut best = u64::MAX;
+    let n = buttons.len();
+
+    for subset in 0..(1u64 << n) {
+        let mut state = 0u64;
+
+        (0..n).for_each(|i| {
+            if (subset >> i) & 1 == 1 {
+                state ^= buttons[i];
+            }
+        });
+
+        if state == mask {
+            let presses = subset.count_ones() as u64;
+            best = best.min(presses);
+        }
+    }
+    best
+}
+
+fn init_square_bitmask(square: &str) -> u64 {
+    let mut mask = 0u64;
+    for (i, c) in square.chars().enumerate() {
+        if c == '#' {
+            mask |= 1 << i;
+        }
+    }
+    mask
+}
+
+fn build_mask(button: &[usize]) -> u64 {
+    let mut mask = 0u64;
+    for x in button {
+        mask |= 1 << x;
+    }
+    mask
 }
 
 fn parseline(line: &str) -> LineData {
